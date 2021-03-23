1912d082465ccaff7b3bee4e29730a4b5bbeec13
sharzil <sharzil.js@zeniuseducation.com>
2021-03-23 16:09:14 +0530

    restaurant context and mock data added

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -2,41 +2,41 @@ import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
 import styled from "styled-components/native";
-import { ThemeContext } from "styled-components";
 
+import { SafeArea } from "../../../components/utility/safe-area.component";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
-import { SafeArea } from "../../../components/utility/safe-area.component";
+
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantList = styled(FlatList).attrs((props) => ({
+const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
-    // padding: props.theme.space[3],
     padding: 16,
   },
-}))``;
+})``;
 
 export const RestaurantsScreen = () => {
-  const theme = useContext(ThemeContext);
-  console.log("Current theme: ", theme.space[3]);
-
+  const { isLoading, error, restaurants } = useContext(RestaurantsContext);
+  console.log(error);
   return (
     <SafeArea>
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
       <RestaurantList
-        theme={theme}
-        data={[{ name: 1 }, { name: 2 }, { name: 3 }]}
-        renderItem={() => (
-          <Spacer position="bottom" size="large">
-            <RestaurantInfoCard />
-          </Spacer>
-        )}
-        keyExtractor={(item) => item.name.toString()}
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <Spacer position="bottom" size="large">
+              <RestaurantInfoCard restaurant={item} />
+            </Spacer>
+          );
+        }}
+        keyExtractor={(item) => item.name}
       />
     </SafeArea>
   );
